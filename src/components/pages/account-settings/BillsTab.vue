c3c7303bd71bdbe5fbe7525d204b93b87f37927c
king <xianxin001@gmail.com>
2025-01-06 23:48:02 +0800

    222222

--- src/components/pages/account-settings/BillsTab.vue
+++ src/components/pages/account-settings/BillsTab.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive,onMounted } from 'vue';
+import { ref, reactive } from 'vue';
 import Snackbar from '@/components/dashboards/snackbar.vue';
 
 const snackbarRef: any = ref(null);
@@ -7,18 +7,11 @@ const dataForm = reactive({
     browerApi: '',
 });
 
-onMounted(()=>{
-    let browerUrl = localStorage.getItem('browerApi')
-    if(browerUrl){
-        dataForm.browerApi = browerUrl
-    } ;
-})
-
 const submitHandle = () => {
     if (!dataForm.browerApi) {
         return snackbarRef.value?.open({ type: 'error', message: '信息填写不完整' });
     }
-    localStorage.setItem('browerApi',dataForm.browerApi)
+    localStorage.setItem('browerApi', dataForm.browerApi);
     return snackbarRef.value?.open({ type: 'success', message: '更新成功了' });
 };
 </script>
@@ -28,12 +21,21 @@ const submitHandle = () => {
         <v-col cols="12" md="9">
             <v-card elevation="10">
                 <v-card-item>
-                    <h5 class="text-h5">浏览器地址接口</h5>
-                    <div class="text-subtitle-1 text-grey100 mt-2">指纹浏览器调用接口</div>
+                    <h5 class="text-h5">IP地址接口</h5>
+                    <div class="text-subtitle-1 text-grey100 mt-2">默认调用接口</div>
                     <div class="mt-5">
                         <v-label class="mb-2 font-weight-medium">地址</v-label>
-                        <v-text-field color="primary" variant="outlined" v-model="dataForm.browerApi" />
+                        <v-text-field color="primary" placeholder="例如：e3954635a37ce335.lqz.na.ipidea.online:2333" variant="outlined" v-model="dataForm.browerApi" />
                     </div>
+                    <div class="mt-5">
+                        <v-label class="mb-2 font-weight-medium">账号</v-label>
+                        <v-text-field color="primary" placeholder="账号" variant="outlined" v-model="dataForm.browerApi" />
+                    </div>
+                    <div class="mt-5">
+                        <v-label class="mb-2 font-weight-medium">密码</v-label>
+                        <v-text-field color="primary" placeholder="密码" variant="outlined" v-model="dataForm.browerApi" />
+                    </div>
+
                     <div class="d-flex justify-center mt-5 pb-3">
                         <v-btn size="large" @click="submitHandle" color="primary" class="mr-4" rounded="pill">Save</v-btn>
                     </div>
